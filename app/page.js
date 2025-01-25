b1f189dd5dda1debb193d22ef8ef8ab6d6d90ae2
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2025-01-26 00:23:45 +0530

    Added User Authentication using Clerk and stored the data in convex user DB using mutations

--- app/page.js
+++ app/page.js
@@ -1,13 +1,34 @@
-import Test from '@/components/Test'
-import React from 'react'
+"use client";
+
+import Test from "@/components/Test";
+import { api } from "@/convex/_generated/api";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
+import React, { useEffect } from "react";
 
 const page = () => {
+  const { user } = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    user && CheckUser();
+  }, [user]);
+
+  const CheckUser = async () => {
+    const response = await createUser({
+        userName: user?.fullName,
+        email: user?.primaryEmailAddress?.emailAddress,
+        imageURL: user?.imageUrl,
+    });
+  };
+
   return (
-    <div className='text-3xl font-extrabold text-blue-500'>
+    <div className="text-3xl font-extrabold text-blue-500">
       This is just a test page
       <Test />
+      <UserButton />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- convex/schema.js
+++ convex/schema.js
@@ -3,7 +3,7 @@ import { v } from "convex/values";
 
 export default defineSchema({
   users: defineTable({
-    useerName: v.string(),
+    userName: v.string(),
     email: v.string(),
     imageURL: v.string(),
   }),
